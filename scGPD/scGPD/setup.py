4f7fa04782a7be7f5b9912f65b9a26cb4bf061d9
TinaGuo <spruceyunshan@gmail.com>
2025-06-20 16:11:51 -0400

    Add files via upload

---
+++ scGPD/scGPD/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+requirements = [
+    'numpy',
+    'torch',
+    'tqdm',
+    'scikit-learn',
+    'h5py',
+    'anndata',
+    'scanpy',
+    'matplotlib',
+    'scipy',
+    'pandas',
+    'toml'
+]
+
+setuptools.setup(
+    name='scGPD',
+    version='0.0.1',
+    author='Tina Guo',
+    description='scGPD: single-cell informed Gene Panel Desig',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/TinaGuo/scGPD',
+    packages=['scGPD'],
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering'
+    ],
+    python_requires='>=3.6',
+)
