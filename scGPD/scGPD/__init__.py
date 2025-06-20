604c5a1d80027f63585bc0e7c2fcdd58db1d708e
TinaGuo <spruceyunshan@gmail.com>
2025-06-20 16:14:05 -0400

    Delete scGPD directory

--- scGPD/scGPD/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import data
-from . import utils
-from . import functions
-from . import selection
-from scGPD.selection import scGPD
-from scGPD.data import GeneSet, load_set
-from scGPD.data import ExpressionDataset, HDF5ExpressionDataset
-from scGPD.utils import PoiLoss, MSELoss, Accuracy
